06cf4702ecefa8645533874bd061877c2b9a0611
Thales Liu <32575445+liuzheng1990@users.noreply.github.com>
2018-11-20 12:53:25 +0800

    Update simple_greet_command.ino

--- examples/simple_greet_command/simple_greet_command.ino
+++ examples/simple_greet_command/simple_greet_command.ino
@@ -1,6 +1,6 @@
-#include <SerialCli.h>
+#include <Neutrino.h>
 
-SerialCli sc = SerialCli(true); // if you put true, you can use '_help' command for free.
+Neutrino neu = Neutrino(true); // if you put true, you can use '_help' command for free.
 
 uint8_t greet() // a sample callback function
 { 
@@ -10,8 +10,8 @@ uint8_t greet() // a sample callback function
 
 void setup() {
   // put your setup code here, to run once:
-  sc.add_command("hello", greet); // register function greet to command name "hello".
-  sc.print_help_info();
+  neu.add_command("hello", greet); // register function greet to command name "hello".
+  neu.print_help_info();
 }
 
 void loop() {
@@ -20,7 +20,7 @@ void loop() {
   while (!Serial.available()); // empty loop, wait for serial command
 
   command = Serial.readString(); //store the command request to command
-  retval = sc.parse_command(command);
+  retval = neu.parse_command(command);
   Serial.print("Command finished running. Returned value: ");
   Serial.println(retval);
 }
